5f037f7a6c0c2fa064284372095596e798ff5252
Robbert <robbertluit@hotmail.com>
2019-02-14 14:57:55 +0100

    routing

--- delenconnect/routes/web.php
+++ delenconnect/routes/web.php
@@ -1,20 +1,19 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/', 'HomeController@index')
+    ->middleware('auth')
+    ->name('delen-connect-home');;
+
+Route::get('/overzicht-gebeurtenissen', 'HomeController@index')
+    ->middleware('auth')
+    ->name('delen-connect-gebeurtenissen');
+
+Route::get('/{?customer}/mijn-tijdlijn', 'HomeController@timelineCustomer')
+    ->middleware('auth')
+    ->name('tijdlijn-voor-klant');
+
+Route::get('/{?account-manager}/mijn-klanten', 'HomeController@customers')
+    ->middleware('auth')
+    ->name('tijdlijn-voor-account-manager');
