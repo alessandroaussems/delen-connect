9ad80492bc6364ab16a4426169d2c004a500ab27
Robbert <robbertluit@hotmail.com>
2019-02-14 16:24:33 +0100

    controller login

--- delenconnect/app/Http/Controllers/Auth/LoginController.php
+++ delenconnect/app/Http/Controllers/Auth/LoginController.php
@@ -4,17 +4,20 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
     use AuthenticatesUsers;
+
+    protected $user = '';
 
     /**
      * Where to redirect users after login.
      *
      * @var string
      */
-    protected $redirectTo = '/';
+    protected $redirectTo = '/my-timeline';
 
     /**
      * Create a new controller instance.
