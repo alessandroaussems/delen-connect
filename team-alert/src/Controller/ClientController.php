ff674f2f7af68526604e70fc54ca7fd3a8862cfc
Robbert <robbertluit@hotmail.com>
2019-02-14 22:57:01 +0100

    .

--- team-alert/src/Controller/ClientController.php
+++ team-alert/src/Controller/ClientController.php
@@ -15,4 +15,13 @@ class ClientController extends BaseController
         return $this->render('partials/timeline.html.twig', array('customers' => $customers)
         );
     }
+
+    public function overview()
+    {
+        $customers = $this->container->get('doctrine')->getManager()->getRepository('App:Customer')->findBy(
+            array()
+        );
+        return $this->render('partials/client_overview.html.twig', array('customers' => $customers)
+        );
+    }
 }
