2789f4d26d2924de5139b7f97a5a2665a97ec396
Robbert <robbertluit@hotmail.com>
2019-02-14 16:26:35 +0100

    merge

--- delenconnect/public/js/app.js
+++ delenconnect/public/js/app.js
@@ -49091,8 +49091,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /Users/alessandroaussems/Sites/teamalert/delenconnect/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /Users/alessandroaussems/Sites/teamalert/delenconnect/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /Users/louise/Documents/School/teamalert/delenconnect/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /Users/louise/Documents/School/teamalert/delenconnect/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
