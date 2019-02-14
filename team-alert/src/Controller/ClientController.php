35eddb925b441b966e7d50876bfd7af6daca5d15
Robbert <robbertluit@hotmail.com>
2019-02-14 21:58:03 +0100

    img fixes

--- team-alert/src/Controller/ClientController.php
+++ team-alert/src/Controller/ClientController.php
@@ -6,7 +6,7 @@ use Symfony\Component\HttpFoundation\Request;
 
 class ClientController extends BaseController
 {
-    public function index(Request $request, $category = null)
+    public function index(Request $request)
     {
         return $this->render('default/index.html.twig', array()
         );
