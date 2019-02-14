a32786c64760701818db0392c83260ad486f0c70
alessandroaussems <me@alessandroaussems.be>
2019-02-14 18:13:19 +0100

    Save Work

--- delenconnect/resources/views/timeline.blade.php
+++ delenconnect/resources/views/timeline.blade.php
@@ -17,6 +17,19 @@
                     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque dolorem est magnam perferendis placeat recusandae similique. Amet architecto, consequatur, earum enim eos fuga impedit maiores nemo, possimus quia quod temporibus.</p>
                 </main>
             </li>
+            <li class="event"><span>Afspraak: ma 10 dec. 2019</span></li>
+            <li class="account">
+                <header>
+                    <img src="../img/person.png" alt="">
+                    <section>
+                        <small>ma 10 dec. 2019</small>
+                        <p>Alexandre Delen</p>
+                    </section>
+                </header>
+                <main>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque dolorem est magnam perferendis placeat recusandae similique. Amet architecto, consequatur, earum enim eos fuga impedit maiores nemo, possimus quia quod temporibus.</p>
+                </main>
+            </li>
         </ul>
         <aside>
         </aside>
