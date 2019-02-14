d52ab63b6f5d0568154f16d223d3bbf16970e40b
Robbert <robbertluit@hotmail.com>
2019-02-14 22:18:55 +0100

    stuff

--- team-alert/src/Controller/ClientController.php
+++ team-alert/src/Controller/ClientController.php
@@ -8,6 +8,10 @@ class ClientController extends BaseController
 {
     public function index(Request $request)
     {
+        $customers = $this->container->get('doctrine')->getManager()->getRepository('App:Customer')->findBy(
+            array()
+        );
+        dump($customers);
         return $this->render('default/index.html.twig', array()
         );
     }
